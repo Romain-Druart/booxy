f84b149da5d604feda40e6525328e08356e167ba
LakunCindy <cindy.lakun@gmail.com>
2022-02-13 19:36:07 +0100

    black header responsive to tablet

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,17 +12,6 @@ module.exports = {
     fontFamily: {
       'sans': ['Open Sans', 'ui-sans-serif', 'system-ui'],
     },
-    screens: {
-      'tablet': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'laptop': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'desktop': '1280px',
-      // => @media (min-width: 1280px) { ... }
-    },
-
   },
   plugins: [
     require('@tailwindcss/forms'),
